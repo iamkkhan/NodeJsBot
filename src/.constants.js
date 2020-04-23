cee88810a9fd9714196e5c12e7c8c7db2350fe4d
iamkkhan <62652500+iamkkhan@users.noreply.github.com>
2020-04-23 16:33:51 +0530

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,15 +1,15 @@
 module.exports = Object.freeze({
-  TOKEN: '946438606:AAFEI_QUW6qTGjQeI1oNxbGiPIID18sXuM8', //required
-  ARIA_SECRET: 'bhadoo', //change as per needs.
+  TOKEN: '1089171832:AAHKxlCwhB8ijDlCgbTMnXsmBabRDSQU6mc', //required
+  ARIA_SECRET: 'default', //change as per needs.
   ARIA_DOWNLOAD_LOCATION: '/app/downloads)', //-leave as it is
-  INDEX_DOMAIN: 'https://workers.workers.dev/', // replace with your index domain. use https://github.com/ParveenBhadooOfficial/go-drive
+  INDEX_DOMAIN: '', // replace with your index domain. use https://github.com/ParveenBhadooOfficial/go-drive
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION -leave as it is
   ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings -leave as it is
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded -leave as it is
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well -leave as it is
-  GDRIVE_PARENT_DIR_ID:  '1A3K2M4qSHmA4VZpOMUWWHweDyJBLAcgF', // for directorty eg. https://drive.google.com/drive/folders/1p7pHhSh2yboWePmo53DnVr3xkAYiojP9 use 1p7pHhSh2yboWePmo53DnVr3xkAYiojP9
-  SUDO_USERS: [646146866],	// Telegram user IDs. These users can use the bot in any chat. required, get it using https://t.me/YourTGid_Bot
-  AUTHORIZED_CHATS: [-1001350343006],	// Telegram chat IDs. Anyone in these chats can use the bot. Add https://t.me/YourTGid_Bot to Group and click /id
+  GDRIVE_PARENT_DIR_ID:  '18mv-DZIkkIL6ZuxTi0NCg6F-zgaL4Xli', // for directorty eg. https://drive.google.com/drive/folders/1p7pHhSh2yboWePmo53DnVr3xkAYiojP9 use 1p7pHhSh2yboWePmo53DnVr3xkAYiojP9
+  SUDO_USERS: [1030618484],	// Telegram user IDs. These users can use the bot in any chat. required, get it using https://t.me/YourTGid_Bot
+  AUTHORIZED_CHATS: [-411331611],	// Telegram chat IDs. Anyone in these chats can use the bot. Add https://t.me/YourTGid_Bot to Group and click /id
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting -leave as it is
   DRIVE_FILE_PRIVATE: {
     ENABLED: false,
@@ -23,8 +23,9 @@ module.exports = Object.freeze({
   },
     COMMANDS_USE_BOT_NAME: {
       ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command.
-      NAME: "@Trollmusicbot" //Enter your Bot Username here if above is set to true.
-    }
+      NAME: "" //Enter your Bot Username here if above is set to true.
+    },
+  IS_TEAM_DRIVE: true
 });
 
 // already deployed in @BhadooCloud in Telegram.
